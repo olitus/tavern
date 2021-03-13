e65889292f99e1c120670a65e019ef37246d0a81
Oleksandr Litus <oleks.litus@gmail.com>
2021-03-13 04:16:31 -0500

    Clean

--- Tavern/TavernApp.swift
+++ Tavern/TavernApp.swift
@@ -17,15 +17,3 @@ struct TavernApp: App {
         .windowStyle(HiddenTitleBarWindowStyle())
     }
 }
-
-
-
-//
-//# tavern
-//Independent app manger for Mac based on brew
-//
-//
-//### Major
-//- https://github.com/Homebrew/brew
-//- https://github.com/brunophilipe/Cakebrew
-//- https://github.com/andrewn/brew-services-menubar
